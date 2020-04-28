4246941cdc7dfb67664537c81088d11117ce04b2
nullx <mail@nullx.me>
2020-04-28 22:18:08 +0200

    Initial Commit

---
+++ windowDetector/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  windowDetector
+//
+//  Created by Jon Lara Trigo on 28/04/2020.
+//  Copyright © 2020 Jon Lara Trigo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
